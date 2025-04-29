4e08ab11a5bb123406a94efb8f1161ee62374319
yesukumar bhuvanagiri <kumaryesu2000@gmail.com>
2025-04-29 10:09:47 +0530

    initial folder structure done
    initial Folder Structure build is done, Read the readme file any doubts.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
+import About from "./pages/About/About";
+import Management from "./pages/Management/Management";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Router>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+
+        <main className="flex-grow">
+          <Routes>
+            {/* Default route */}
+            {/* <Route path="/" element={<Home />} /> */}
+            {/* About page Routes */}
+            <Route path="/about" element={<About />} />
+            {/* Management page Routes */}
+            <Route path="/management" element={<Management />} />
+            {/* Add more routes as needed */}
+          </Routes>
+        </main>
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </Router>
     </>
   )
 }
---
+++ client/src/pages/About/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Vision from './Vision/Vision'
+
+function About() {
+  return (
+    <>
+    <div>About</div>
+    <Vision/>
+    </>
+  )
+}
+
+export default About
