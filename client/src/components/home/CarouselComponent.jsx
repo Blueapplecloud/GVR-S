11b5b36c46936d241a01f7fba3f32d575d30e8c1
Anvesh_1130 <anveshkankanala1130@gmail.com>
2025-04-29 13:14:13 +0530

    Add Carousel and Highlights components to Home page
    - Implemented CarouselComponent for image carousel with fade effect and auto-slide functionality.
    - Created Highlights component showcasing key features with icons, descriptions, and buttons.
    - Integrated both components into the Home page for a cohesive layout.

---
+++ client/src/components/home/CarouselComponent.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+import Carousel from "react-bootstrap/Carousel";
+
+// Array of carousel items with image URL, title, and description
+const carouselItems = [
+  {
+    imageUrl: "/images/carousel/img1.jpg",
+  },
+];
+
+function CarouselComponent() {
+  return (
+    <Carousel
+      fade
+      controls={true} // Show arrows
+      indicators={true} // Show pagination dots
+      interval={3000} // Auto-slide every 3 seconds
+      pause="hover" // Pause on hover
+      wrap={true} // Infinite loop
+    >
+      {carouselItems.map((item, index) => (
+        <Carousel.Item key={index}>
+          <img
+            className="d-block w-100 object-cover h-auto"
+            src={item.imageUrl}
+            alt={item.altText}
+          />
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+}
+
+export default CarouselComponent;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CarouselComponent from "../components/home/CarouselComponent";
+import Highlights from "../components/home/Highlights";
+
+function Home() {
+  return (
+    <div>
+      <CarouselComponent />
+      <Highlights />
+    </div>
+  );
+}
+
+export default Home;
