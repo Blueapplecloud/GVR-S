2d6c99f116eb6801dd82bd92d9ecb310f9ff7886
Anvesh_1130 <anveshkankanala1130@gmail.com>
2025-05-01 13:16:28 +0530

    Refactor CSS imports and clean up main.jsx structure

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,13 +9,12 @@
 //   </StrictMode>,
 // )
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css"; // Ensure Tailwind CSS is imported
+import App from "./App";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css'; // Ensure Tailwind CSS is imported
-import App from './App';
- 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
