b1b179ea2580cc0f0c648482f1fa8ea32f2d6c72
Anvesh_1130 <anveshkankanala1130@gmail.com>
2025-05-01 09:19:21 +0530

    Revert "merging changes form Anvesh branch to main branch"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,14 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
+import './App.css'
 import About from "./pages/About/About";
 import NavbarComponent from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import AboutGVRS from "./pages/About/About GVR&S/About GVR&S";
 import VisionMission from "./pages/About/Vision&Mission/Vision&Mission";
 import PrincipalMessage from "./pages/About/Principal Message/Principal's Message";
-
-import Home from "./pages/Home";
-import FeeStructure from "./pages/Academics/FeeStructure";
-import AcademicScholarShip from "./pages/Academics/AcademicScholarShip";
-import AcademicRegulations from "./pages/Academics/AcademicRegulations";
-
 import Founder from "./pages/Management/Founder/Founder";
 import CSE from "./pages/Departments/UG/CSE/CSE";
 import EEE from "./pages/Departments/UG/EEE/EEE";
@@ -27,50 +21,10 @@ import SH from "./pages/Departments/UG/S&H/S&H";
 
 
 function App() {
+
   return (
     <>
       <Router>
-
-        <div>
-          <CustomNavbar />
-
-          <main>
-            <Routes>
-              {/* Default route */}
-              {/* <Route path="/" element={<CustomNavbar />} /> */}
-              {/* Home page Routes */}
-              <Route path="/" element={<Home />} />
-              {/* About page Routes */}
-              <Route path="/about" element={<About />} />
-              {/* Management page Routes */}
-              <Route path="/management" element={<Management />} />
-              {/* Add more routes as needed */}
-              <Route path="/about/history" element={<AboutGVRS />} />
-              <Route path="/about/vision&mission" element={<VisionMission />} />
-              <Route path="/about/message" element={<PrincipalMessage />} />
-
-              {/* Academics page Routes */}
-
-              <Route
-                path="/academics/fee-structure"
-                element={<FeeStructure />}
-              />
-              <Route
-                path="/academics/academic-scholarship"
-                element={<AcademicScholarShip />}
-              />
-              <Route
-                path="/academics/academic-regulations"
-                element={<AcademicRegulations />}
-              />
-
-              {/* <Route path="/academics/academic-calender" element={<AcademicCalender />} /> */}
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
-
       <div>
         <NavbarComponent />
 
@@ -100,9 +54,8 @@ function App() {
         <Footer />
       </div>
     </Router>
-
     </>
-  );
+  )
 }
 
-export default App;
+export default App
