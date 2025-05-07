4d3a90053a2c059e3c5f6e7ea86a8681866dadc2
yesukumar bhuvanagiri <kumaryesu2000@gmail.com>
2025-05-07 11:44:25 +0530

    update 01-Refactor code structure for improved readability and added updated images and responsive in all screens

--- client/src/pages/Management/Dean/Dean.jsx
+++ client/src/pages/Management/Dean/Dean.jsx
@@ -4,11 +4,11 @@ import { useInView } from "react-intersection-observer";
 
 function Dean() {
   const { ref, inView } = useInView({ triggerOnce: true, threshold: 0.1 });
+
   return (
-    <div className="container m-10 bg-white shadow-lg rounded-md ">
+    <div className="container mx-auto my-6 px-4 py-6 bg-white shadow-lg rounded-md">
       <motion.h1
         ref={ref}
-        // key={index}
         initial={{ opacity: 0, y: -20 }}
         animate={inView ? { opacity: 1, y: 0 } : {}}
         transition={{ duration: 1 }}
@@ -18,63 +18,66 @@ function Dean() {
         <div className="w-[7rem] h-0.5 bg-Amber mt-2"></div>
       </motion.h1>
 
-      <div>
-        <p>
-          It is my privilege and pleasure to welcome you all to GVR&S College of
-          Engineering and Technology.
-        </p>
-        <div className="float-end m-5 mt-0 text-center">
-          <img
-            src="https://gvrs.ac.in/wp-content/uploads/2023/06/dean-248x300.jpg"
-            alt="Dr. Vara Prasad"
-            className="h-90 rounded-lg shadow-lg mx-auto"
-          />
+      <div className="flex flex-col lg:flex-row lg:items-start lg:space-x-8">
+        {/* Text Content */}
+        <div className="flex-1 text-justify">
+          <p>
+            It is my privilege and pleasure to welcome you all to GVR&S College of
+            Engineering and Technology.
+          </p>
+          <p className="mt-4">
+            It is an Institute where discipline and punctuality with conducive
+            environment will provide quality education and development in frontier
+            areas of engineering and technology. We, the faculty, staff and
+            administration at GVR&S will work for producing technologically
+            superior and ethically strong engineers for the country and the world
+            with a purpose to serve the society & mankind. With strong team work
+            we would achieve technological excellence in a highly competitive
+            environment around us.
+          </p>
+          <p className="mt-4">
+            As we strive for excellence in what we do, the Institute is constantly
+            making endeavors to scale new heights by developing synergy between
+            academic knowledge, technical skill set in line with industry needs,
+            high moral values and sensitivity to the environment and the nation.
+            Therefore apart from academics, co-curricular activities, sports and
+            cultural development will form an important part of the life at GVR&S
+            from the very beginning.
+          </p>
+          <p className="mt-4">
+            I wish our students to soar high by excelling in academics and other
+            activities upholding the name of GVR&S and to bring fame to the
+            institution with their sterling qualities. In an era of stiff
+            competition, success can only be achieved if one makes the right kind
+            of endeavor at the right time and in the right direction. “Success
+            will never lower its standard to accommodate us; we have to raise our
+            standard to achieve it”.
+          </p>
+          <p className="mt-4">
+            With this I invite you to visit GVR&S to experience the difference.
+          </p>
+
+          <div className="mt-6">
+            <a
+              href="/management/dean"
+              className="border border-Amber bg-primaryColor hover:bg-Amber text-white px-10 py-3 text-lg font-sans font-semibold rounded-md hover:!text-black transition duration-200 ease-in-out"
+            >
+              View More &gt;&gt;
+            </a>
+          </div>
+        </div>
+
+        {/* Image */}
+        <div className="mt-6 lg:mt-0 lg:w-72 text-center">
+          <div className="shadow-lg rounded-lg overflow-hidden mx-auto">
+            <img
+              src="https://gvrs.ac.in/wp-content/uploads/2023/06/dean-248x300.jpg"
+              alt="Dr. Vara Prasad"
+               className="w-full h-auto"
+            />
+          </div>
           <p className="mt-3 font-semibold text-Red">Dr. Vara Prasad, Dean</p>
         </div>
-        <p className="text-justify">
-          It is an Institute where discipline and punctuality with conducive
-          environment will provide quality education and development in frontier
-          areas of engineering and technology. We, the faculty, staff and
-          administration at GVR&S will work for producing technologically
-          superior and ethically strong engineers for the country and the world
-          with a purpose to serve the society & mankind. With strong team work
-          we would achieve technological excellence in a highly competitive
-          environment around us.
-        </p>
-        <p className="text-justify">
-          As we strive for excellence in what we do, the Institute is constantly
-          making endeavors to scale new heights by developing synergy between
-          academic knowledge, technical skill set in line with industry needs,
-          high moral values and sensitivity to the environment and the nation.
-          Therefore apart from academics, co-curricular activities, sports and
-          cultural development will form an important part of the life at GVR&S
-          from the very beginning.
-        </p>
-        <p className="text-justify">
-          I wish our students to soar high by excelling in academics and other
-          activities upholding the name of GVR&S and to bring fame to the
-          institution with their sterling qualities. In an era of stiff
-          competition, success can only be achieved if one makes the right kind
-          of endeavor at the right time and in the right direction. “Success
-          will never lower its standard to accommodate us; we have to raise our
-          standard to achieve it”.
-        </p>
-        <p className="text-justify">
-          With this I invite you to visit GVR&S to experience the difference.
-        </p>
-        <p className="mt-5"></p>
-        <br />
-        <a
-  href="https://gvrs.ac.in/academic-dean/"
-className="border border-Amber bg-primaryColor rounded hover:bg-Amber text-white px-10 py-3 text-lg font-sans font-semibold rounded-md  hover:!text-black transition duration-200 ease-in-out p-2 mx-1"
-                
->
-  View More &gt;&gt;
-</a>
-
-
-        <br />
-        <br />
       </div>
     </div>
   );
