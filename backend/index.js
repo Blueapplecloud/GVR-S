398ffbf8911679e173782c490841ad480324ba68
Anvesh_1130 <anveshkankanala1130@gmail.com>
2025-05-09 17:41:08 +0530

    added controllers , routes , model Add user registration and login functionality with MongoDB integration in the backend

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 require("dotenv").config();
+const UserRoute = require("./routes/User.route");
+const cors = require("cors");
+
+const connectToDB = require("./DB/connectToDB");
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/users", UserRoute);
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log("Server is running on port 3000");
+const port = process.env.PORT || 5000;
+
+app.listen(port, async () => {
+  await connectToDB();
+  console.log("Server is running on port " + port);
 });
