34fc4e8b7b2b31b23d55842a2456f3988b378b8e
Anvesh_1130 <anveshkankanala1130@gmail.com>
2025-05-13 13:29:01 +0530

    feat: enhance SideNav layout and add AdminBackdrop component for logout functionality and also  added refactored the login component

--- client/src/Reusable Components/SideNav.jsx
+++ client/src/Reusable Components/SideNav.jsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import AdminBackdrop from "../components/Admin/Backdrop";
 
- function SideNav() {
+function SideNav() {
   const links = [
-    { name: 'Placements', path: '/admin/placements' },
-    { name: 'Students', path: '/admin/students' },
-    { name: 'Events', path: '/admin/events' },
+    { name: "Placements", path: "/admin/placements" },
+    { name: "Students", path: "/admin/students" },
+    { name: "Events", path: "/admin/events" },
   ];
 
   return (
-    <aside className="w-64 bg-white shadow-md h-screen p-4 ">
+    <aside className="w-64 flex flex-col bg-white shadow-md h-screen p-4 ">
       <h2 className="text-xl font-bold mb-6">Admin Panel</h2>
-      <nav className="flex flex-col space-y-3">
-        {links.map(link => (
+      <nav className="flex flex-col flex-grow space-y-3 mb-3">
+        {links.map((link) => (
           <NavLink
             key={link.name}
             to={link.path}
             className={({ isActive }) =>
               `py-2 px-4 rounded-lg transition ${
-                isActive ? 'bg-primaryColor text-white' : 'text-gray-700 hover:bg-gray-100'
+                isActive
+                  ? "bg-primaryColor text-white"
+                  : "text-gray-700 hover:bg-gray-100"
               }`
             }
           >
@@ -26,6 +29,9 @@ import { NavLink } from 'react-router-dom';
           </NavLink>
         ))}
       </nav>
+      <div className="flex items-center  mt-auto">
+        <AdminBackdrop />
+      </div>
     </aside>
   );
 }
